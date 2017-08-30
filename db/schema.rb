67211f83760cc6c208272eec8c0dca307c7b7232
Ross Ewing <rmewing129@gmail.com>
2017-08-30 17:24:43 +0000

    Added create action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170829235748) do
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.text     "comment"
+    t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
